964c115e1926fdea2ab67e973fe20ccf6d8e171a
alexOm98 <95749189+alexOm98@users.noreply.github.com>
2022-05-23 21:41:26 +0300

    fix: dir removal related errors

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,26 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 function copyDir () {
-  fs.mkdir(path.join(__dirname, 'files-copy'), { recursive : true }, (err) => {
+  fs.rm(path.join(__dirname, 'files-copy'),  { recursive: true, force: true}, (err) => {
     if (err) throw err;
-  });
-  fs.readdir(path.join(__dirname, 'files-copy'), (err, files) => {
-    if (err) throw err;
-    files.forEach(file => {
-      fs.unlink(path.join(__dirname, 'files-copy', file), (err) => {
+    fs.mkdir(path.join(__dirname, 'files-copy'), { recursive : true }, (err) => {
+      if (err) throw err;
+      fs.readdir(path.join(__dirname, 'files'), (err, files) => {
         if (err) throw err;
+        else{
+          files.forEach(file => {
+            fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), (err) => {
+              if (err) throw err;
+            });
+          });
+        }
       });
     });
   });
-  fs.readdir(path.join(__dirname, 'files'), (err, files) => {
-    if (err) throw err;
-    else{
-      files.forEach(file => {
-        fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file) ,(err) => {
-          if (err) throw err;
-        });
-      });
-    }
-  });
 }
-copyDir();
+copyDir ();
--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,18 +3,18 @@ const path = require('path');
 function mergeStyles () {
   fs.writeFile(path.join(__dirname, 'project-dist', 'bundle.css'), '', function (err) {
     if (err) throw err;
-  });
-  fs.readdir(path.join(__dirname, 'styles'), (err, files) => {
-    if (err) throw err;
-    files.forEach(file => {
-      if(file.slice(-4) === '.css'){
-        const readableStream  = fs.createReadStream(path.join(__dirname, 'styles', file));
-        readableStream.on('data', chunk => {
-          fs.appendFile(path.join(__dirname, 'project-dist', 'bundle.css'), chunk.toString(), (err) => {
-            if (err) throw err;
+    fs.readdir(path.join(__dirname, 'styles'), (err, files) => {
+      if (err) throw err;
+      files.forEach(file => {
+        if(file.slice(-4) === '.css'){
+          const readableStream  = fs.createReadStream(path.join(__dirname, 'styles', file));
+          readableStream.on('data', chunk => {
+            fs.appendFile(path.join(__dirname, 'project-dist', 'bundle.css'), chunk.toString(), (err) => {
+              if (err) throw err;
+            });
           });
-        });
-      }
+        }
+      });
     });
   });
 }
